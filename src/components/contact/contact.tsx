7bace9155586152bd8f86afce72dccf1bdfddb6a
Wesley Silva <wesveio@gmail.com>
2022-09-29 18:21:17 -0300

    get contacts

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,23 +1,44 @@
-import { component$ } from '@builder.io/qwik';
+import {
+  component$,
+  createContext,
+  useContextProvider,
+  useStore,
+  useContext,
+} from '@builder.io/qwik';
 
+export const ContactsContext = createContext('Contacts');
 
 export const openContacts = async () => {
-    const props = ['name', 'email', 'tel', 'address', 'icon'];
-    const opts = { multiple: true };
-    const supported = 'contacts' in navigator && 'ContactsManager' in window;
+  const props = ['name', 'email', 'tel', 'address', 'icon'];
+  const opts = { multiple: true };
+  const supported = 'contacts' in navigator && 'ContactsManager' in window;
+  const contacts = useContext(ContactsContext) as any;
   if (supported) {
-    const nav = navigator as any
-    const contacts = await nav?.contacts?.select(props, opts);
-    console.log('CONTACTS', contacts);
+    const nav = navigator as any;
+    const contactsSelected = await nav?.contacts?.select(props, opts);
+    contacts.push(...contactsSelected)
+    console.log('SELECTED CONTACTS', contactsSelected);
   }
 };
 
-export const Contact = component$((props: any) => {
-  const { contacts } = props;
+export const Contact = component$(() => {
+    useContextProvider(
+      ContactsContext,
+      useStore({
+        contacts: [],
+      })
+    );
+
+    return <ContactComponent />
+  });
+
+export const ContactComponent = component$(() => {
+  const contacts = useContext(ContactsContext) as any;
+  console.log("CONTACTS", contacts)
   return (
     <>
-      {contacts ? (
-        <p>{JSON.stringify(contacts)}</p>
+      {contacts.length ? (
+        <p>{JSON.stringify(contacts?.name)}</p>
       ) : (
         <button onClick$={() => openContacts()} class='button button--green'>
           Contacts 📡
