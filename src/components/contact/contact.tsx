446352b6ca25b56bb66cd9be2c291a72aa74aab9
Wesley Silva <wesveio@gmail.com>
2022-09-30 12:01:54 -0300

    passing contacts as props

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -8,23 +8,23 @@ import {
 
 export const ContactsContext = createContext('Contacts');
 
-export const openContacts = component$(() => {
+export const openContacts = (contacts:any) => {
   const props = ['name', 'email', 'tel', 'address', 'icon'];
   const opts = { multiple: true };
   const supported = 'contacts' in navigator && 'ContactsManager' in window;
-  const contacts = useContext(ContactsContext) as any;
+//   const contacts = useContext(ContactsContext) as any;
   console.log('openContacts', contacts);
   
   if (supported) {
     const nav = navigator as any;
     nav?.contacts?.select(props, opts).then((data:any)=>{
         console.log('SELECTED CONTACTS', data);
+        // contacts.push(...data)
     })
-    // contacts.push(...contactsSelected)
     // console.log('STORE CONTACTS', contacts);
   }
   return <></>
-});
+};
 
 export const Contact = component$(() => {
     useContextProvider(
@@ -45,7 +45,7 @@ export const ContactComponent = component$(() => {
       {contacts.length ? (
         <p>{JSON.stringify(contacts?.name)}</p>
       ) : (
-        <button onClick$={() => openContacts} class='button button--green'>
+        <button onClick$={() => openContacts(contacts)} class='button button--green'>
           Contacts 📡
         </button>
       )}
