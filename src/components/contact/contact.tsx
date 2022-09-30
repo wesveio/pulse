295ec8348030882e52e8cc8cdfbff96333f65847
Wesley Silva <wesveio@gmail.com>
2022-09-30 12:39:24 -0300

    mod render

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -9,47 +9,49 @@ import {
 export const ContactsContext = createContext('Contacts');
 
 export const Contact = component$(() => {
-    useContextProvider(
-      ContactsContext,
-      useStore({
-        contacts: [],
-      })
-    );
+  useContextProvider(
+    ContactsContext,
+    useStore({
+      contacts: [],
+    })
+  );
 
-    return <ContactComponent />
-  });
+  return <ContactComponent />;
+});
 
-  export const openContacts = (contacts:any) => {
-    const props = ['name', 'email', 'tel', 'address', 'icon'];
-    const opts = { multiple: true };
-    const supported = 'contacts' in navigator && 'ContactsManager' in window;
-    console.log('openContacts', contacts.contacts);
-    
-    if (supported) {
-      const nav = navigator as any;
-      nav?.contacts?.select(props, opts).then((data:any)=>{
-          console.log('SELECTED CONTACTS', data);
-          contacts.contacts.push(...data)
-          console.log('STORE CONTACTS', contacts);
-      })
-    }
-  };
+export const openContacts = (contacts: any) => {
+  const props = ['name', 'email', 'tel', 'address', 'icon'];
+  const opts = { multiple: true };
+  const supported = 'contacts' in navigator && 'ContactsManager' in window;
+  console.log('openContacts', contacts.contacts);
+
+  if (supported) {
+    const nav = navigator as any;
+    nav?.contacts?.select(props, opts).then((data: any) => {
+      console.log('SELECTED CONTACTS', data);
+      contacts.contacts.push(...data);
+      console.log('STORE CONTACTS', contacts);
+    });
+  }
+};
 
 export const ContactComponent = component$(() => {
   const contacts = useContext(ContactsContext) as any;
-  console.log("CONTACTS", contacts)
-
-  
+  console.log('CONTACTS', contacts);
 
   return (
     <>
-      {contacts.length ? (
-        <p>Teste</p>
-      ) : (
-        <button onClick$={() => openContacts(contacts)} class='button button--green'>
-          Contacts 📡
-        </button>
-      )}
+      <button
+        onClick$={() => openContacts(contacts)}
+        class='button button--green'
+      >
+        Contacts 📡
+      </button>
+      {
+        contacts.length && (
+            <p>Teste</p>
+        )
+      }
     </>
   );
 });
