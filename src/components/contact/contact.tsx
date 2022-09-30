b008ddd11c304d6b963fcc80b92fc75ceb803354
Wesley Silva <wesveio@gmail.com>
2022-09-30 12:36:28 -0300

    mod contacts

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -8,23 +8,6 @@ import {
 
 export const ContactsContext = createContext('Contacts');
 
-export const openContacts = (contacts:any) => {
-  const props = ['name', 'email', 'tel', 'address', 'icon'];
-  const opts = { multiple: true };
-  const supported = 'contacts' in navigator && 'ContactsManager' in window;
-//   const contacts = useContext(ContactsContext) as any;
-  console.log('openContacts', contacts);
-  
-  if (supported) {
-    const nav = navigator as any;
-    nav?.contacts?.select(props, opts).then((data:any)=>{
-        console.log('SELECTED CONTACTS', data);
-        contacts.push(...data)
-    })
-    console.log('STORE CONTACTS', contacts);
-  }
-};
-
 export const Contact = component$(() => {
     useContextProvider(
       ContactsContext,
@@ -36,13 +19,32 @@ export const Contact = component$(() => {
     return <ContactComponent />
   });
 
+  export const openContacts = (contacts:any) => {
+    const props = ['name', 'email', 'tel', 'address', 'icon'];
+    const opts = { multiple: true };
+    const supported = 'contacts' in navigator && 'ContactsManager' in window;
+    console.log('openContacts', contacts.contacts);
+    
+    if (supported) {
+      const nav = navigator as any;
+      nav?.contacts?.select(props, opts).then((data:any)=>{
+          console.log('SELECTED CONTACTS', data);
+          contacts.contacts.push(...data)
+          console.log('STORE CONTACTS', contacts);
+      })
+    }
+  };
+
 export const ContactComponent = component$(() => {
   const contacts = useContext(ContactsContext) as any;
   console.log("CONTACTS", contacts)
+
+  
+
   return (
     <>
       {contacts.length ? (
-        <p>{JSON.stringify(contacts?.name)}</p>
+        <p>Teste</p>
       ) : (
         <button onClick$={() => openContacts(contacts)} class='button button--green'>
           Contacts 📡
